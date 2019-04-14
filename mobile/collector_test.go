19f3a3428855aef00b36a1f683a69ce59cabc5e4
Simone Basso <bassosimone@gmail.com>
2019-04-14 23:33:24 +0200

    Add tests for report resubmission

---
+++ mobile/collector_test.go
@@ -0,0 +1,38 @@
+package mobile
+
+import (
+	"fmt"
+	"testing"
+)
+
+const origMeasurement = `{
+	"data_format_version": "0.2.0",
+	"input": "torproject.org",
+	"measurement_start_time": "2016-06-04 17:53:13",
+	"probe_asn": "AS0",
+	"probe_cc": "ZZ",
+	"probe_ip": "127.0.0.1",
+	"software_name": "measurement_kit",
+	"software_version": "0.2.0-alpha.1",
+	"test_keys": {
+		"failure": null,
+		"received": [],
+		"sent": []
+	},
+	"test_name": "tcp_connect",
+	"test_runtime": 0.253494024276733,
+	"test_start_time": "2016-06-04 17:53:13",
+	"test_version": "0.0.1"
+}`;
+
+func TestCollectorResubmitIntegration(t *testing.T) {
+	settings := &MKECollectorResubmitSettings{}
+	settings.SetSerializedMeasurement(origMeasurement)
+	settings.SetTimeout(14)
+	results := settings.Perform()
+	fmt.Println(results.Logs())
+	fmt.Println(results.UpdatedSerializedMeasurement())
+	if !results.Good() {
+		t.Fatal("resubmission failed")
+	}
+}
