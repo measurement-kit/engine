46b30d3b90c929d11ba8ddaf363161ed582beb53
Simone Basso <bassosimone@gmail.com>
2019-04-17 17:18:49 +0200

    bouncer: ensure we have reasonable coverage

--- bouncer/bouncer_test.go
+++ bouncer/bouncer_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 )
 
+// TestGetCollectorsIntegration just fetches the collectors.
 func TestGetCollectorsIntegration(t *testing.T) {
 	entries, err := GetCollectors(context.Background(), Config{
 		BaseURL: "https://bouncer.ooni.io/",
@@ -15,6 +16,17 @@ func TestGetCollectorsIntegration(t *testing.T) {
 	}
 }
 
+// TestGetCollectorsFailure checks we deal with a failure.
+func TestGetCollectorsFailure(t *testing.T) {
+	_, err := GetCollectors(context.Background(), Config{
+		BaseURL: "\t", // this should be an invalid URL
+	})
+	if err == nil {
+		t.Fatal("We expected an error here")
+	}
+}
+
+// TestGetTestHelpersIntegration just fetches the test helpers.
 func TestGetTestHelpersIntegration(t *testing.T) {
 	entries, err := GetTestHelpers(context.Background(), Config{
 		BaseURL: "https://bouncer.ooni.io/",
@@ -22,5 +34,15 @@ func TestGetTestHelpersIntegration(t *testing.T) {
 	t.Logf("%+v", entries)
 	if err != nil {
 		t.Fatal(err)
+	}
+}
+
+// TestGetTestHelpersFailure checks we deal with a failure.
+func TestGetTestHelpersFailure(t *testing.T) {
+	_, err := GetTestHelpers(context.Background(), Config{
+		BaseURL: "\t", // this should be an invalid URL
+	})
+	if err == nil {
+		t.Fatal("We expected an error here")
 	}
 }
