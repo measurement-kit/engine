033d3cde6c0676e6a8abfc8a3db55864b25c3c70
Simone Basso <bassosimone@gmail.com>
2019-04-27 02:29:45 +0200

    Rewrite build script and improve generated libs

---
+++ build.bash
@@ -0,0 +1,35 @@
+#!/bin/sh
+set -ex
+
+engine="github.com/measurement-kit/engine"
+pkgs=`echo ${engine}/{collector,geoip,task}`
+
+echo time gomobile bind                             \
+  -target android                              \
+  -o mkengine.aar                              \
+  -javapkg io.ooni.mk.engine                   \
+  -ldflags="-s -w"                             \
+  ${pkgs}
+
+time gomobile bind                             \
+  -target ios                                  \
+  -o MKEngine.framework                        \
+  -prefix MKE                                  \
+  -ldflags="-s -w"                             \
+  ${pkgs}
+
+versionA=MKEngine.framework/Versions/A
+
+mv ${versionA}/{Collector,MKEngine}
+
+cat ${versionA}/Headers/Collector.h                               \
+  | sed 's/__Collector_FRAMEWORK_H__/__MKEngine_FRAMEWORK_H__/g'  \
+    > ${versionA}/Headers/MKEngine.h
+rm ${versionA}/Headers/Collector.h
+
+cat ${versionA}/Modules/module.modulemap \
+  | sed 's/Collector/MKEngine/g' > ${versionA}/Modules/module.modulemap.new
+mv ${versionA}/Modules/module.modulemap{.new,}
+
+rm MKEngine.framework/Collector
+ln -s Versions/Current/MKEngine MKEngine.framework/MKEngine
